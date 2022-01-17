f40b19b8c3a7648524cb619ef50329ba3466f6d6
shobasuresh <65395553+shobasuresh@users.noreply.github.com>
2022-01-17 20:34:10 +0530

    Version 3

---
+++ CollectionsDemo/src/main/java/com/fidelity/countrypack/Address.java
@@ -0,0 +1,27 @@
+package com.fidelity.countrypack;
+
+public class Address {
+	
+	private String cityyyy;
+	private String state;
+	
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	 public String toString()
+	 {
+		 
+		 return city +" "+state;
+	 }
+}
